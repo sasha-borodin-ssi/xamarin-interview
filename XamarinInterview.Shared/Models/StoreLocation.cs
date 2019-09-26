4b3c35926b0125672de8fa7d46b68cf493b1d2da
Sasha Borodin <sasha.borodin@smartstartinc.com>
2019-09-26 12:14:36 -0500

    Starting template.

---
+++ XamarinInterview.Shared/Models/StoreLocation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace XamarinInterview.Shared.Models
+{
+    public class StoreLocation
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
---
+++ XamarinInterview.Shared/StoreLocationsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace XamarinInterview.Shared
+{
+    public class StoreLocationsService
+    {
+		readonly string apiKey = "96A81537-4FF2-4947-A31E-CDE3B4A5F1D1";
+		readonly string countryISOCode = "US";
+		readonly string companyId = "7FE2D98C-FA28-443D-8A66-38F9ADBDB296";
+
+		public async Task<List<StoreLocation>> SearchByZipCode(string zipCode)
+		{
+			// TODO: implement call to REST API
+
+			// documentation of the REST endpoint can be viewed via Swagger UI at:
+			// https://webapidev.smartstartinc.com/swagger/ui/index#!/StoreLocations/StoreLocations_LookupByZip
+
+			// there is 1 required request header that isn't documented in the Swagger UI:
+			// Authorization: Bearer <api key>
+
+			return new List<StoreLocation>();
+		}
+	}
+}
