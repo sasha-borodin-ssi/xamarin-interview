b3d58f5a56abb0e77189d5a88ba159ddda1623e5
Sasha Borodin <sasha.borodin@smartstartinc.com>
2019-09-27 23:33:05 -0500

    A few more dependency tweaks and hints.

--- XamarinInterview.Shared/StoreLocation.cs
+++ XamarinInterview.Shared/StoreLocation.cs
@@ -11,5 +11,12 @@ namespace XamarinInterview.Shared
 
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+
+
+        public string CityState => $"{City}, {State}";
     }
 }
--- XamarinInterview.Shared/StoreLocationsService.cs
+++ XamarinInterview.Shared/StoreLocationsService.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace XamarinInterview.Shared
 {
     public class StoreLocationsService
     {
+        readonly string apiUrl = "https://webapidev.smartstartinc.com";
 		readonly string apiKey = "96A81537-4FF2-4947-A31E-CDE3B4A5F1D1";
+
 		readonly string countryISOCode = "US";
 		readonly string companyId = "7FE2D98C-FA28-443D-8A66-38F9ADBDB296";
 
